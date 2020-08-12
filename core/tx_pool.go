26709bb260bf9c046da3334f9a2c3f651d5131d1
villanuevawill <hello@willvillanueva.com>
2020-08-12 11:06:19 -0700

    Check time before db lookup

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -569,12 +569,12 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 		return ErrUnderpriced
 	}
 	// Ensure the transaction adheres to nonce ordering
+	validationTimer.UpdateSince(start)
 	if pool.currentState.GetNonce(from) > tx.Nonce() {
 		return ErrNonceTooLow
 	}
 	// Transactor should have enough funds to cover the costs
 	// cost == V + GP * GL
-	validationTimer.UpdateSince(start)
 	if pool.currentState.GetBalance(from).Cmp(tx.Cost()) < 0 {
 		return ErrInsufficientFunds
 	}
