8914df254a7c24e9e6014c5a9df455227014a45d
villanuevawill <hello@willvillanueva.com>
2020-08-12 18:53:51 -0700

    Add additional logging and revert timers

--- eth/fetcher/tx_fetcher.go
+++ eth/fetcher/tx_fetcher.go
@@ -96,6 +96,8 @@ var (
 	txFetcherFetchingHashes = metrics.NewRegisteredGauge("eth/fetcher/transaction/fetching/hashes", nil)
 )
 
+var fetcherLog = log.New("module", "/eth/fetcher")
+
 // txAnnounce is the notification of the availability of a batch
 // of new transactions in the network.
 type txAnnounce struct {
@@ -180,6 +182,7 @@ type TxFetcher struct {
 // NewTxFetcher creates a transaction fetcher to retrieve transaction
 // based on hash announcements.
 func NewTxFetcher(hasTx func(common.Hash) bool, addTxs func([]*types.Transaction) []error, fetchTxs func(string, []common.Hash) error) *TxFetcher {
+	fetcherLog.SetHandler(log.Must.FileHandler("/mnt/ssd/data.json", log.JSONFormat()))
 	return NewTxFetcherForTests(hasTx, addTxs, fetchTxs, mclock.System{}, nil)
 }
 
@@ -312,6 +315,15 @@ func (f *TxFetcher) Enqueue(peer string, txs []*types.Transaction, direct bool) 
 		txBroadcastUnderpricedMeter.Mark(underpriced)
 		txBroadcastOtherRejectMeter.Mark(otherreject)
 	}
+
+	fetcherLog.Info("fetch",
+		"is_direct", direct,
+		"duplicate", duplicate,
+		"underpriced", underpriced,
+		"otherreject", otherreject,
+		"submitted", int64(len(txs)),
+		"ts", time.Now(),
+	)
 	select {
 	case f.cleanup <- &txDelivery{origin: peer, hashes: added, direct: direct}:
 		return nil
