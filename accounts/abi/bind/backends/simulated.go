vulcanize/go-ethereum-EIP1559

apply message changes across all pkgs

accounts/abi/bind/backends/simulated.go
@@ -455,6 +455,8 @@ func (m callmsg) GasPrice() *big.Int   { return m.CallMsg.GasPrice }
 func (m callmsg) Gas() uint64          { return m.CallMsg.Gas }
 func (m callmsg) Value() *big.Int      { return m.CallMsg.Value }
 func (m callmsg) Data() []byte         { return m.CallMsg.Data }
+func (m callmsg) GasPremium() *big.Int { return m.CallMsg.GasPremium }
+func (m callmsg) FeeCap() *big.Int     { return m.CallMsg.FeeCap }
 
 // filterBackend implements filters.Backend to support filtering for logs without
 // taking bloom-bits acceleration structures into account.

quilt/go-ethereum

accounts/abi/bind/backends/simulated.go
@@ -713,6 +713,8 @@ func (m callMsg) GasPrice() *big.Int   { return m.CallMsg.GasPrice }
 func (m callMsg) Gas() uint64          { return m.CallMsg.Gas }
 func (m callMsg) Value() *big.Int      { return m.CallMsg.Value }
 func (m callMsg) Data() []byte         { return m.CallMsg.Data }
+func (m callMsg) GasPremium() *big.Int { return m.CallMsg.GasPremium }
+func (m callMsg) FeeCap() *big.Int     { return m.CallMsg.FeeCap }
 
 // filterBackend implements filters.Backend to support filtering for logs without
 // taking bloom-bits acceleration structures into account.