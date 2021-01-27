07288f9a20b1f6a32fdc7566d125e9a061e6b4e7
lightclient@protonmail.com <lightclient@protonmail.com>
2021-01-27 01:51:16 +0100

    all: e2e test for eip-1559 plus some bug fixes

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -194,8 +194,8 @@ func (s eip2930Signer) Sender(tx *Transaction) (common.Address, error) {
 	case AccessListTxType:
 		fallthrough
 	case DynamicFeeTxType:
-		// ACL txs are defined to use 0 and 1 as their recovery id, add
-		// 27 to become equivalent to unprotected Homestead signatures.
+		// ACL and DynamicFee txs are defined to use 0 and 1 as their recovery
+		// id, add 27 to become equivalent to unprotected Homestead signatures.
 		V = new(big.Int).Add(V, big.NewInt(27))
 	default:
 		return common.Address{}, ErrTxTypeNotSupported
@@ -215,6 +215,14 @@ func (s eip2930Signer) SignatureValues(tx *Transaction, sig []byte) (R, S, V *bi
 			V.Add(V, s.chainIdMul)
 		}
 	case *AccessListTx:
+		// Check that chain ID of tx matches the signer. We also accept ID zero here,
+		// because it indicates that the chain ID was not specified in the tx.
+		if txdata.ChainID.Sign() != 0 && txdata.ChainID.Cmp(s.chainId) != 0 {
+			return nil, nil, nil, ErrInvalidChainId
+		}
+		R, S, _ = decodeSignature(sig)
+		V = big.NewInt(int64(sig[64]))
+	case *DynamicFeeTransaction:
 		// Check that chain ID of tx matches the signer. We also accept ID zero here,
 		// because it indicates that the chain ID was not specified in the tx.
 		if txdata.ChainID.Sign() != 0 && txdata.ChainID.Cmp(s.chainId) != 0 {
