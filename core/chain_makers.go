0c8445c006f3e8fb79b2688ff080bfd3d12040a9
lightclient@protonmail.com <lightclient@protonmail.com>
2021-01-27 01:51:16 +0100

    all: e2e test for eip-1559 plus some bug fixes

--- core/chain_makers.go
+++ core/chain_makers.go
@@ -248,7 +248,7 @@ func makeHeader(chain consensus.ChainReader, parent *types.Block, state *state.S
 		time = parent.Time() + 10 // block time is fixed at 10 seconds
 	}
 
-	return &types.Header{
+	header := &types.Header{
 		Root:       state.IntermediateRoot(chain.Config().IsEIP158(parent.Number())),
 		ParentHash: parent.Hash(),
 		Coinbase:   parent.Coinbase(),
@@ -262,6 +262,14 @@ func makeHeader(chain consensus.ChainReader, parent *types.Block, state *state.S
 		Number:   new(big.Int).Add(parent.Number(), common.Big1),
 		Time:     time,
 	}
+
+	if chain.Config().IsTokyo(parent.Number()) {
+		header.BaseFee = misc.CalcBaseFee(parent.Header())
+	} else if chain.Config().IsTokyo(header.Number) {
+		header.BaseFee = new(big.Int).SetUint64(params.InitialBaseFee)
+	}
+
+	return header
 }
 
 // makeHeaderChain creates a deterministic chain of headers rooted at parent.
